f94455f9ff1e6ec1f03bf10dfbeffbb49c1d9914
Manishkatel <manishkatel2020@gmail.com>
2026-09-03 14:07:54 -0500

    customer completed by sushant-manish

--- customer.py
+++ customer.py
@@ -12,23 +12,56 @@ class Customer:
 
     def __init__(self):
         """
-        Initialize all stored data for the object
+        Initializing all stored data for the object
         """
-        pass
+        self.name = ""
+        self.address = ""
+        self.city = ""
+        self.state = ""
+        self.zip = ""
+        self.telephone = ""
+        self.account_balance = 0.0
+        self.date_last_payment = ""
 
     def to_list(self):
         """
         Returns the customer data as a list for CSV file
         """
-        pass
-    def from_list(self):
+        return [
+            self.name,
+            self.address,
+            self.city,
+            self.state,
+            self.zip,
+            self.telephone,
+            self.account_balance,
+            self.date_last_payment
+        ]
+
+    def from_list(self, data):
         """
         Creates customer object from CSV file
         """
-        pass
+        self.name = data[0]
+        self.address = data[1]
+        self.city = data[2]
+        self.state = data[3]
+        self.zip = data[4]
+        self.telephone = data[5]
+        self.account_balance = float(data[6])
+        self.date_last_payment = data[7]
 
     def __str__(self):
         """
         Return a string representation of an object
         """
-        pass
+        return (
+            f"Name: {self.name}\n"
+            f"Address: {self.address}\n"
+            f"City: {self.city}\n"
+            f"State: {self.state}\n"
+            f"Zip: {self.zip}\n"
+            f"Telephone Number: {self.telephone}\n"
+            f"Account Balance: ${self.account_balance:.2f}\n"
+            f"Date of Last Payment: {self.date_last_payment}"
+        )
